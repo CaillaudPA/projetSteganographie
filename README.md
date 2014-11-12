90149a154feee124b5e5b5a5821e60b4b5471c77
Loic BEAULIEU <E134756Z@U-INF-J-E114-12.iut-nantes.univ-nantes.prive>
2014-11-12 11:10:55 +0100

    ajout stegano_image

--- README.md
+++
@@ -1,4 +0,0 @@
-projetSteganographie
-====================
-
-Projet semestriel
---
+++ src/classeStatic/Stegano_image.java
@@ -0,0 +1,25 @@
+
+public class Stegano_image extends Steganographie{
+
+	public Stegano_image(String lettre, String enveloppe){
+		super(lettre,enveloppe);
+	}
+
+	//i le numeros de l'algo a utilisé
+	//1= remplacer le bit de poids faible de chaque couleur
+	public void dissimulerDonnee(int i){
+
+	}
+
+	//i le numéros de l'algo utilise lorsque le message a été cacher
+	public void devoilerDonnee(int i){
+
+	}
+
+	public boolean verificationComptabilite(){
+
+	}
+	
+
+
+}
